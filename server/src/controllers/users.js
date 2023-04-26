7a9d218779a5f05e7ac4523f0c34e795c254f335
Parvizkh8 <33002107+Parvizkh8@users.noreply.github.com>
2023-04-26 14:26:31 +0200

    email sent successfully for registration

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,8 +1,11 @@
+// const fs = require('fs');
 const registerUser = async (req, res, next) => {
     try {
         const { name, email, password, phone } = req.body;
+
         console.log(req.body);
         console.log(req.file);
+
         res.status(200).json({ message: 'email was sent' })
     } catch (error) {
         res.json({
