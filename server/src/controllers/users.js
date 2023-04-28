8f8335017335bade1aecc6522aae1aeadc654869
Parvizkh8 <33002107+Parvizkh8@users.noreply.github.com>
2023-04-28 07:38:03 +0200

    create error handling

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -1,17 +1,28 @@
+const { errorResponse } = require("../utils/responsehandler");
+const createError = require('http-errors');
+
 // const fs = require('fs');
 const registerUser = async (req, res, next) => {
     try {
         const { name, email, password, phone } = req.body;
 
-        console.log(req.body);
-        console.log(req.file);
+        // when we are throwing any error the catch will handle it
+        //  and catch will call the next middleware
+        if (!name || !email || !password || !phone) throw createError(404,
+            'name, email, password or phone is missing');
+        if (password.length < 6) throw createError(400,
+            'password length should be at least 6 characters');
+
+        const image = req.file;
+        if (image && image.size > Math.pow(1024, 1))
+            throw createError(400,
+                'file is too large. It must be less than 1 mb in size');
 
         res.status(200).json({ message: 'email was sent' })
     } catch (error) {
-        res.json({
-            message: error.message
-        });
-    }
-};
+        next(error)
+    };
+}
 
 module.exports = { registerUser }
+
--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const cors = require('cors');
 const morgan = require('morgan');
+const createError = require('http-errors')
+
 
 // const usersRouter = require('./routes/users');
 const { dev } = require('./config');
@@ -37,9 +39,19 @@ app.get('/test', (req, res) => {
 
 // error middleware from express
 app.use((req, res, next) => {
-    res.status(404).send('route Not found');
+    // calling the http-error (the final error) from here
+    // it basically means that we want to fo to the next
+    // middleware but this one has the error with status code of 404 and so on
+    next(createError(404, 'Not found'))
 });
+// next() - we can call this function from everywhere by the http-error package
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('something broke');
+    // the line below says if there is an error which is created by createError
+    // the res.status will be err.status, otherwise it is a server error (500)
+    ; res.status(err.status || 500).json({
+        error: {
+            status: err.status || 500,
+            message: err.message
+        }
+    });
 });
